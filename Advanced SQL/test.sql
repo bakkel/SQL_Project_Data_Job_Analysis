e5705eb10c058d6fe80463a153505aa82c463f01
Bakkel <michel@bakkel.com>
2024-03-29 21:22:49 +0100

    Added Manipulate Tables chapter

--- Advanced SQL/test.sql
+++ Advanced SQL/test.sql
@@ -1,3 +1,6 @@
-SELECT *
+SELECT 
+    job_title_short AS title,
+    job_location AS location,
+    job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'UTC +1'
 FROM job_postings_fact
-LIMIT 200
+LIMIT 5;
