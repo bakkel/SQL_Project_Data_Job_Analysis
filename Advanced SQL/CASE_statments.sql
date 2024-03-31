af6513d368fabba54c8fc598e6d0f6c91f03283d
Bakkel <michel@bakkel.com>
2024-03-31 13:02:34 +0200

    Added 2nd Try Problem # date_funtions & added CASE_statements

---
+++ Advanced SQL/CASE_statments.sql
@@ -0,0 +1,45 @@
+-- Problem 1
+SELECT
+    job_id,
+    job_title,
+    salary_year_avg,
+    CASE
+        WHEN salary_year_avg > 100000 THEN 'High Salary'
+        WHEN salary_year_avg BETWEEN 60000 AND 99999 THEN 'Standard Salary'
+        WHEN salary_year_avg < 60000 THEN 'Low Salary'
+    END AS salary_category
+FROM
+    job_postings_fact
+WHERE
+    salary_year_avg IS NOT NULL
+    AND job_title_short = 'Data Analyst'
+ORDER BY
+    salary_year_avg DESC;
+
+-- Problem 2
+SELECT
+    COUNT(DISTINCT CASE WHEN job_work_from_home = TRUE THEN company_id END) AS wfh_companies,
+    COUNT(DISTINCT CASE WHEN job_work_from_home = FALSE THEN company_id END) AS non_wfh_companies
+FROM
+    job_postings_fact;
+
+-- Problem 3
+SELECT
+    job_id,
+    salary_year_avg,
+    CASE
+        WHEN job_title ILIKE '%Senior%' THEN 'Senior'
+        WHEN job_title ILIKE '%Lead%' OR job_title ILIKE '%Manager%' THEN 'Lead/Manager'
+        WHEN job_title ILIKE '%Junior%' OR job_title ILIKE '%Entry%' THEN 'Junior/Entry'
+        ELSE 'Not Specified'
+    END AS experience_level,
+    CASE
+        WHEN job_work_from_home = TRUE  THEN 'Yes'
+        WHEN job_work_from_home = FALSE  THEN 'No'
+    END AS remote_option
+FROM
+    job_postings_fact
+WHERE
+    salary_year_avg IS NOT NULL
+ORDER BY
+    job_id
--- Advanced SQL/date_functions.sql
+++ Advanced SQL/date_functions.sql
@@ -1,3 +1,4 @@
+-- Problem 1
 SELECT 
     job_schedule_type,
      AVG(salary_year_avg) AS year_avg,
@@ -8,6 +9,7 @@ WHERE
 GROUP BY job_schedule_type
 ORDER BY job_schedule_type;
 
+-- Problem 2
 SELECT
 	EXTRACT(MONTH FROM job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York') AS month,
     COUNT(*) AS job_posted_count
@@ -16,6 +18,7 @@ FROM
 GROUP BY month
 ORDER BY month;
 
+-- 1e poging Problem 3
 SELECT
     company_dim.name AS company_name,
     COUNT(job_id) AS job_postings_count
@@ -32,3 +35,22 @@ HAVING
     COUNT(job_id) > 0
 ORDER BY
     job_postings_count DESC;
+
+-- 2e poging Problem 3
+SELECT
+    company_dim.name AS Company_name,
+    COUNT(job_postings_fact.job_id) AS job_count
+FROM job_postings_fact
+    INNER JOIN 
+    company_dim ON
+    job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_postings_fact.job_health_insurance = TRUE
+    AND
+    EXTRACT(QUARTER FROM job_postings_fact.job_posted_date) = 2
+GROUP BY
+    company_dim.name
+HAVING
+    COUNT(job_postings_fact.job_id) > 0
+ORDER BY
+    job_count DESC
