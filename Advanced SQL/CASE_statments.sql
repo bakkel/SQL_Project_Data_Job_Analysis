dae9c0250f5e74accf94f1249949d564b48a36b8
Bakkel <michel@bakkel.com>
2024-03-31 13:20:58 +0200

    Added Problem 3 Alternative 0n CASE_statments

--- Advanced SQL/CASE_statments.sql
+++ Advanced SQL/CASE_statments.sql
@@ -42,4 +42,27 @@ FROM
 WHERE
     salary_year_avg IS NOT NULL
 ORDER BY
-    job_id
+    job_id;
+
+-- Problem 3 Alternative
+SELECT
+    job_postings.job_id,
+    companies.name,
+    job_postings.salary_year_avg,
+    CASE
+        WHEN job_postings.job_title ILIKE '%Senior%' THEN 'Senior'
+        WHEN job_postings.job_title ILIKE '%Lead%' OR job_postings.job_title ILIKE '%Manager%' THEN 'Lead/Manager'
+        WHEN job_postings.job_title ILIKE '%Junior%' OR job_postings.job_title ILIKE '%Entry%' THEN 'Junior/Entry'
+        ELSE 'Not Specified'
+    END AS experience_level,
+    CASE
+        WHEN job_postings.job_work_from_home = TRUE  THEN 'Yes'
+        WHEN job_postings.job_work_from_home = FALSE  THEN 'No'
+    END AS remote_option
+FROM job_postings_fact AS job_postings
+LEFT JOIN company_dim AS companies 
+	ON job_postings.company_id = companies.company_id
+WHERE
+    job_postings.salary_year_avg IS NOT NULL
+ORDER BY
+    job_postings.job_id
